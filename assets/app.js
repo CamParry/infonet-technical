e6e4f638f09ac0f1de84abf528d2ad9ad5cc5274
Cam Parry <64690031+CamParry@users.noreply.github.com>
2024-06-16 14:15:24 +0200

    Remove bug notes

--- assets/app.js
+++ assets/app.js
@@ -9,11 +9,8 @@ import "./styles/app.css";
 
 document.addEventListener("DOMContentLoaded", function () {
 	// set image src on file upload change
-	// BUG: issue with symfony not reloading the page
 	const fileUpload = document.querySelector('.picture input[type="file"]');
-	console.log(fileUpload);
 	fileUpload.addEventListener("change", function () {
-		console.log(fileUpload.files[0]);
 		const image = document.querySelector(".picture img");
 		const reader = new FileReader();
 		reader.onload = function (e) {
