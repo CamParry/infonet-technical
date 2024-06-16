4c5bd7c5354f0496e32dfaf0a6cd88aa515be7c9
Cam Parry <64690031+CamParry@users.noreply.github.com>
2024-06-16 14:12:13 +0200

    Notes about image preview bug

--- assets/app.js
+++ assets/app.js
@@ -9,16 +9,16 @@ import "./styles/app.css";
 
 document.addEventListener("DOMContentLoaded", function () {
 	// set image src on file upload change
-	document
-		.querySelectorAll('.picture input[type="file"]')
-		.forEach(function (fileUpload) {
-			fileUpload.addEventListener("change", function () {
-				const image = document.querySelector(".picture img");
-				const reader = new FileReader();
-				reader.onload = function (e) {
-					image.src = e.target.result;
-				};
-				reader.readAsDataURL(fileUpload.files[0]);
-			});
-		});
+	// BUG: issue with symfony not reloading the page
+	const fileUpload = document.querySelector('.picture input[type="file"]');
+	console.log(fileUpload);
+	fileUpload.addEventListener("change", function () {
+		console.log(fileUpload.files[0]);
+		const image = document.querySelector(".picture img");
+		const reader = new FileReader();
+		reader.onload = function (e) {
+			image.src = e.target.result;
+		};
+		reader.readAsDataURL(fileUpload.files[0]);
+	});
 });
